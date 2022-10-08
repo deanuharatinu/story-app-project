005b3ff90831e3ee701ab54b0b2e7af7164aa005
deanuharatinu <deanu.alt@gmail.com>
2022-10-08 23:48:25 +0700

    Add robolectric dependencies. Add testing for AddStoryViewModelTest.

---
+++ app/src/test/java/com/deanu/storyapp/addstory/AddStoryViewModelTest.kt
@@ -0,0 +1,96 @@
+package com.deanu.storyapp.addstory
+
+import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.deanu.storyapp.TestCoroutineRule
+import com.deanu.storyapp.common.data.api.model.ApiAddNewStoryResponse
+import com.deanu.storyapp.common.data.utils.MockitoHelper.anyObject
+import com.deanu.storyapp.common.data.utils.getImageAsset
+import com.deanu.storyapp.common.domain.repository.StoryAppRepository
+import com.deanu.storyapp.common.utils.DispatchersProvider
+import com.deanu.storyapp.getOrAwaitValue
+import com.haroldadmin.cnradapter.NetworkResponse
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito.*
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import retrofit2.Response
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(RobolectricTestRunner::class)
+class AddStoryViewModelTest {
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
+
+    private val repository: StoryAppRepository = mock(StoryAppRepository::class.java)
+    private lateinit var addStoryViewModel: AddStoryViewModel
+    private val token =
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJ1c2VyLVp4d0gweFFPZTdLcUdtN1giLCJpYXQiOjE2NjQxMDYzNDl9.I6YRyyLmaB76CQX5sV9_Lesf9qs7r0nDUWDTi8PlHH8"
+    private val response = mock(Response::class.java)
+    private val responseSuccess = ApiAddNewStoryResponse(false, "success")
+    private val responseError = ApiAddNewStoryResponse(true, "failed")
+
+    @Before
+    fun setup() {
+        val dispatchersProvider = object : DispatchersProvider {
+            override fun io(): CoroutineDispatcher = testCoroutineRule.testDispatcher
+        }
+        `when`(repository.getLoginState()).thenReturn(flowOf(token))
+        addStoryViewModel = AddStoryViewModel(repository, dispatchersProvider)
+    }
+
+    @Test
+    fun `when add new story is success, addNewStoryResponse message is success`() = runTest {
+        // Given
+        val expectedValue = "success"
+        val response = NetworkResponse.Success<ApiAddNewStoryResponse, ApiAddNewStoryResponse>(
+            responseSuccess,
+            response
+        )
+        val app = RuntimeEnvironment.getApplication() as Application
+        val imageFile = getImageAsset("thunderbird.png", app.applicationContext)
+        `when`(repository.addNewStory(anyObject(), anyObject(), anyObject()))
+            .thenReturn(response)
+
+        // When
+        addStoryViewModel.addNewStory("description text", imageFile)
+        val actualValue = addStoryViewModel.addNewStoryResponse.getOrAwaitValue()
+
+        // Then
+        verify(repository).addNewStory(anyObject(), anyObject(), anyObject())
+        assertEquals(expectedValue, actualValue.message)
+    }
+
+    @Test
+    fun `when add new story is failed, addNewStoryResponse message is failed`() = runTest {
+        // Given
+        val expectedValue = "failed"
+        val response = NetworkResponse.ServerError<ApiAddNewStoryResponse, ApiAddNewStoryResponse>(
+            responseError,
+            response
+        )
+        val app = RuntimeEnvironment.getApplication() as Application
+        val imageFile = getImageAsset("thunderbird.png", app.applicationContext)
+        `when`(repository.addNewStory(anyObject(), anyObject(), anyObject()))
+            .thenReturn(response)
+
+        // When
+        addStoryViewModel.addNewStory("description text", imageFile)
+        val actualValue = addStoryViewModel.addNewStoryResponse.getOrAwaitValue()
+
+        // Then
+        verify(repository).addNewStory(anyObject(), anyObject(), anyObject())
+        assertEquals(expectedValue, actualValue.message)
+    }
+}
---
+++ app/src/test/java/com/deanu/storyapp/common/data/utils/Util.kt
@@ -0,0 +1,33 @@
+package com.deanu.storyapp.common.data.utils
+
+import android.content.Context
+import com.deanu.storyapp.common.utils.createCustomTempFile
+import org.mockito.Mockito
+import java.io.*
+
+fun getImageAsset(filename: String, context: Context): File {
+    try {
+        val fileOut = createCustomTempFile(context)
+
+        val inputStream: InputStream = context.assets.open("image/$filename")
+        val outputStream: OutputStream = FileOutputStream(fileOut)
+        val buf = ByteArray(1024)
+        var len: Int
+        while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+        outputStream.close()
+        inputStream.close()
+
+        return fileOut
+    } catch (exception: IOException) {
+        throw exception
+    }
+}
+
+object MockitoHelper {
+    fun <T> anyObject(): T {
+        Mockito.any<T>()
+        return uninitialized()
+    }
+    @Suppress("UNCHECKED_CAST")
+    fun <T> uninitialized(): T =  null as T
+}
